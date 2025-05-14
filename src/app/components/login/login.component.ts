666bd4f987f43c93b6dc3ecb56d4ece468d7645a
2393163 <2393163@cognizant.com>
2025-05-14 22:54:33 +0530

    second

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  standalone: true
 })
 export class LoginComponent {
-
-}
+  loginForm:FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required,Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(6)]]
+    });
+  }
+  onsubmit() {
+    if (this.loginForm.valid) {
+      alert('Form Submitted!');
+    } else {
+      alert('Form is invalid');
+    }
+  }}
